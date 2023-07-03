12373f50174edcae322427790290b20fb7f93df4
ParthPatadia <74972078+ParthPatadia@users.noreply.github.com>
2023-07-03 11:00:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from wtforms import StringField, SubmitField
 
 def load_vocab():
     vocab = {}
-    with open('E:\\Algozenith\\AZ Hackathon\\vocab.txt', 'r') as f:
+    with open('vocab.txt', 'r') as f:
         vocab_terms = f.readlines()
-    with open('E:\Algozenith\AZ Hackathon\idf-values.txt', 'r') as f:
+    with open('idf-values.txt', 'r') as f:
         idf_values = f.readlines()
     
     for (term,idf_value) in zip(vocab_terms, idf_values):
@@ -23,7 +23,7 @@ def load_vocab():
 
 def load_documents():
     documents = []
-    with open('E:\Algozenith\AZ Hackathon\documents.txt', 'r') as f:
+    with open('documents.txt', 'r') as f:
         documents = f.readlines()
     documents = [document.strip().split() for document in documents]
 
@@ -33,7 +33,7 @@ def load_documents():
 
 def load_inverted_index():
     inverted_index = {}
-    with open('E:\Algozenith\AZ Hackathon\inverted-index.txt', 'r') as f:
+    with open('inverted-index.txt', 'r') as f:
         inverted_index_terms = f.readlines()
 
     for row_num in range(0,len(inverted_index_terms),2):
@@ -45,7 +45,7 @@ def load_inverted_index():
     return inverted_index
 
 def load_link_of_qs():
-    with open("E:\Algozenith\AZ Hackathon\Qindex.txt", "r") as f:
+    with open("Qindex.txt", "r") as f:
         links = f.readlines()
 
     return links
